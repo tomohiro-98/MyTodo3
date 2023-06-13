58aa41d0ca40e4934f6afe393fe4ed353edd9442
tomohiro-98 <tomohiro_kuba@mail.toyota.co.jp>
2023-06-13 14:32:50 +0900

    Initial Commit

---
+++ MyTodo3/MyTodo3App.swift
@@ -0,0 +1,20 @@
+//
+//  MyTodo3App.swift
+//  MyTodo3
+//
+//  Created by user on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct MyTodo3App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
